dcaff1effe64e508cac80d9d64396b52e0252d65
ToDylan <dylan.tosti@gmail.com>
2019-06-13 14:43:08 +0200

    vehicule pres scene

--- Assets/OldProject/OldScripts/TableSc4/Script_Autonomie_clean.cs
+++ Assets/OldProject/OldScripts/TableSc4/Script_Autonomie_clean.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Script_Autonomie : MonoBehaviour
+public class Script_Autonomie_clean : MonoBehaviour
 {
     // ---------- ATTRIBUTS ----------
     public int positionJoueur;
