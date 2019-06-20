d933caef0c0d21e73058af4ca751557f2d7765b3
ToDylan <dylan.tosti@gmail.com>
2019-06-20 13:54:24 +0200

    card move

--- Assets/OldProject/OldScripts/TableSc4/Mouvement_carte.cs
+++ Assets/OldProject/OldScripts/TableSc4/Mouvement_carte.cs
@@ -8,7 +8,10 @@ public class Mouvement_carte : MonoBehaviour
     private Vector3 initialPos;
     private Vector3 offset;
     private Vector3 curPosition;
+    [SerializeField] GameObject target;
+    private bool is_static;
 
+    #region unused start and update
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,7 @@ public class Mouvement_carte : MonoBehaviour
     {
         
     }
+    #endregion
 
     private void OnMouseDown()
     {
@@ -30,8 +34,34 @@ public class Mouvement_carte : MonoBehaviour
 
     private void OnMouseDrag()
     {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
+        if (!IsTheMousInTargetCollider())
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            transform.position = curPosition;
+        }
+        else
+        {
+            gameObject.transform.position = target.transform.position;
+        }
+    }
+    
+    //private void OnCollisionEnter2D(Collision2D collision/*, GameObject selected_card, GameObject type_card_location*/)
+    //{
+    //    if (gameObject.tag == collision.gameObject.tag)
+    //    {
+    //        is_static = true;
+    //        gameObject.transform.position = collision.gameObject.transform.position;
+    //    }
+    //}
+
+    private bool IsTheMousInTargetCollider()
+    {
+        float scale_x = target.GetComponent<BoxCollider2D>().bounds.extents.x;
+        float scale_y = target.GetComponent<BoxCollider2D>().bounds.extents.y;
+        return Input.mousePosition.x > target.transform.position.x - scale_x &&
+               Input.mousePosition.x < target.transform.position.x + scale_x &&
+               Input.mousePosition.y < target.transform.position.y + scale_y &&
+               Input.mousePosition.y < target.transform.position.y + scale_y;
     }
 }
--- Assets/OldProject/OldScripts/TableSc4/Placement_dimension_4_5.cs
+++ Assets/OldProject/OldScripts/TableSc4/Placement_dimension_4_5.cs
@@ -9,30 +9,32 @@ public class Placement_dimension_4_5 : MonoBehaviour
     [SerializeField] GameObject manual_equipment_target;
     [SerializeField] GameObject programmable_equipment_target;
     [SerializeField] GameObject automatic_equipment_target;
-    private float x_position_dimension;
-    private float y_position_dimension;
+    private Mouvement_carte cardmove;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        cardmove = gameObject.GetComponent<Mouvement_carte>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
     {
         
     }
 
     private void OnCollisionEnter2D(Collision2D collision/*, GameObject selected_card, GameObject type_card_location*/)
     {
-        Debug.Log("dimension carte : " + gameObject.tag);
-        Debug.Log("dimension location : " + collision.gameObject.tag);
-
         if(gameObject.tag == collision.gameObject.tag)
         {
-            Debug.Log("ca passssssse");
             gameObject.transform.position = collision.gameObject.transform.position;
+            Debug.Log("position carte : " + gameObject.transform.position);
+            Debug.Log("position target : " + collision.gameObject.transform.position);
         }
     }
 }
