5c6229eb493498697a34b9434b4e3fb386f43d30
ToDylan <dylan.tosti@gmail.com>
2019-07-22 13:56:32 +0200

    vehicule pendant debat

--- Assets/OldProject/OldScripts/TableSc5/PresPersos.cs
+++ Assets/OldProject/OldScripts/TableSc5/PresPersos.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 using System;
+using UnityEditor;
 
 public class PresPersos : MonoBehaviour
 {
@@ -22,9 +23,11 @@ public class PresPersos : MonoBehaviour
     #endregion
 
     #region Inputs
+    [SerializeField] GameObject canvas_pres_vehicule;
     [SerializeField] GameObject canvas_pres_persos;
     [SerializeField] GameObject canvas_debat;
     [SerializeField] GameObject[] persos;
+    [SerializeField] GameObject[] cartes;
     [SerializeField] Button button;
     [SerializeField] Text text;
 	#endregion
@@ -43,6 +46,18 @@ public class PresPersos : MonoBehaviour
 
     void OnEnable()
     {
+        canvas_pres_vehicule.SetActive(true);
+        canvas_pres_vehicule.GetComponent<CanvasScaler>().referenceResolution = new Vector2(10000f, 10000f);
+        canvas_pres_vehicule.transform.GetChild(0).gameObject.SetActive(false);
+        canvas_pres_vehicule.transform.GetChild(1).GetChild(1).gameObject.SetActive(false);
+        canvas_pres_vehicule.GetComponent<Initialisation>().enabled = false;
+        canvas_pres_vehicule.transform.GetChild(1).GetChild(0).GetChild(7).GetComponent< BoxCollider2D>().enabled = false;
+        canvas_pres_vehicule.transform.GetChild(1).GetChild(0).GetChild(7).GetComponent<Mouvement_carte>().enabled = false;
+        foreach (GameObject carte in cartes)
+        {
+            carte.GetComponent<BoxCollider2D>().enabled = false;
+            carte.GetComponent<Mouvement_carte>().enabled = false;
+        }
         if (Partie.Type == "expert")
         {
             fr = "acteur";
@@ -92,39 +107,42 @@ public class PresPersos : MonoBehaviour
 	
     void Update()
     {
-        foreach (Joueur j in Partie.Joueurs)
-        {
-            if (presentateur == j.Numero)
-            {
-                pres = j;
-            }
-            if (GetNextPres(presentateur) == j.Numero)
-            {
-                next = j;
-            }
-        }
+        Debug.Log("update!!!");
+        checkifvehiculeclicked();
 
-        for (int i = 0; i < persos.Length; i++)
-        {
-            if (pres.Position == i)
-            {
-                persos[i].transform.GetChild(0).gameObject.SetActive(true);
-                if (Tour.ZonesDebat[i, 0] != 0)
-                    persos[i].transform.GetChild(Tour.ZonesDebat[i, 0]).gameObject.SetActive(true);
-                if (Tour.ZonesDebat[i, 1] != 0)
-                    persos[i].transform.GetChild(Tour.ZonesDebat[i, 1]).gameObject.SetActive(true);
-            }
-        }
+        //foreach (Joueur j in Partie.Joueurs)
+        //{
+        //    if (presentateur == j.Numero)
+        //    {
+        //        pres = j;
+        //    }
+        //    if (GetNextPres(presentateur) == j.Numero)
+        //    {
+        //        next = j;
+        //    }
+        //}
 
-        if ((GetNextPres(presentateur) == Partie.JoueurCourant) || next.IsPrive)
-            if (Partie.Langue == "FR")
-                button.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Commencer le débat";
-            else
-                button.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Start debate";
-        if (Partie.Langue == "FR")
-            text.text = "Le joueur " + presentateur + " présente son " + fr;
-        else
-            text.text = "Player " + presentateur + " presents their " + en;
+        //for (int i = 0; i < persos.Length; i++)
+        //{
+        //    if (pres.Position == i)
+        //    {
+        //        persos[i].transform.GetChild(0).gameObject.SetActive(true);
+        //        if (Tour.ZonesDebat[i, 0] != 0)
+        //            persos[i].transform.GetChild(Tour.ZonesDebat[i, 0]).gameObject.SetActive(true);
+        //        if (Tour.ZonesDebat[i, 1] != 0)
+        //            persos[i].transform.GetChild(Tour.ZonesDebat[i, 1]).gameObject.SetActive(true);
+        //    }
+        //}
+
+        //if ((GetNextPres(presentateur) == Partie.JoueurCourant) || next.IsPrive)
+        //    if (Partie.Langue == "FR")
+        //        button.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Commencer le débat";
+        //    else
+        //        button.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Start debate";
+        //if (Partie.Langue == "FR")
+        //    text.text = "Le joueur " + presentateur + " présente son " + fr;
+        //else
+        //    text.text = "Player " + presentateur + " presents their " + en;
     }
     #endregion
 
@@ -138,6 +156,19 @@ public class PresPersos : MonoBehaviour
         }
         else
         {
+            canvas_pres_vehicule.transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
+            canvas_pres_vehicule.transform.GetChild(0).gameObject.SetActive(true);
+            canvas_pres_vehicule.transform.GetChild(1).gameObject.SetActive(true);
+            canvas_pres_vehicule.SetActive(false);
+            canvas_pres_vehicule.GetComponent<CanvasScaler>().referenceResolution = new Vector2(1920f, 1080f);
+            canvas_pres_vehicule.GetComponent<Initialisation>().enabled = true;
+            canvas_pres_vehicule.transform.GetChild(1).GetChild(0).GetChild(7).GetComponent<BoxCollider2D>().enabled = true;
+            canvas_pres_vehicule.transform.GetChild(1).GetChild(0).GetChild(7).GetComponent<Mouvement_carte>().enabled = true;
+            foreach (GameObject carte in cartes)
+            {
+                carte.GetComponent<BoxCollider2D>().enabled = true;
+                carte.GetComponent<Mouvement_carte>().enabled = true;
+            }
             MyStringMessage msg = new MyStringMessage();
             NetworkServer.SendToAll(debatID, msg);
             canvas_pres_persos.SetActive(false);
@@ -154,6 +185,26 @@ public class PresPersos : MonoBehaviour
             res = i - 1;
         return res;
     }
+
+    private void checkifvehiculeclicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Input.mousePosition.x < 1000 && Input.mousePosition.x > 900 && 
+                Input.mousePosition.y < 600 && Input.mousePosition.y > 500 && 
+                canvas_pres_vehicule.GetComponent<CanvasScaler>().referenceResolution == new Vector2(10000f, 10000f))
+            {
+                canvas_pres_vehicule.GetComponent<CanvasScaler>().referenceResolution = new Vector2(1920f, 1080f);
+            }
+            else if (Input.mousePosition.x < 1500 && Input.mousePosition.x > 420 && 
+                     Input.mousePosition.y < 1070 && Input.mousePosition.y > 0 &&
+                     canvas_pres_vehicule.GetComponent<CanvasScaler>().referenceResolution == new Vector2(1920f, 1080f))
+            {
+                canvas_pres_vehicule.GetComponent<CanvasScaler>().referenceResolution = new Vector2(10000f, 10000f);
+            }
+        }
+    }
+
     #endregion
 
 }
