fd3eba257bc566a80ace542dc3ffec012f321c82
ToDylan <dylan.tosti@gmail.com>
2019-08-20 10:29:06 +0200

    Revert "attente menu client"
    This reverts commit 75f08bab44108cd36d87e2bd507ba91168eb31c2.

--- Assets/OldProject/OldScripts/AttenteLogo.cs
+++ Assets/OldProject/OldScripts/AttenteLogo.cs
@@ -11,6 +11,8 @@ public class AttenteLogo : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        Debug.Log(Ip_serveur);
+
         string ipv4 = IPManager.GetIP(IPManager.ADDRESSFAM.IPv4);
         if (ipv4 == Ip_serveur)
         {
@@ -18,7 +20,7 @@ public class AttenteLogo : StateMachineBehaviour
         }
         else
         {
-            SceneManager.LoadScene("menu_client");
+            SceneManager.LoadScene("standard_game_client");
         }
     }
 }
